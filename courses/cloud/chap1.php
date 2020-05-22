1550612282a8c4b27c651fb1b489a427632c6557
Wilfridboris <54910937+Wilfridboris@users.noreply.github.com>
2020-05-21 22:24:27 -0400

    Add files via upload

---
+++ courses/cloud/chap1.php
@@ -0,0 +1,6 @@
+<div class="contient">
+    <h1 class="display-4">le Cloud Computing
+        <h2>what to know</h2>
+
+    </h1>
+</div>
---
+++ courses/python/chap1.php
@@ -0,0 +1,5 @@
+<div class="contient">
+    <h1 class="display-4">Python what to know
+
+    </h1>
+</div>
