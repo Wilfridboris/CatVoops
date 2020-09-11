4c49d3f3231b2c8230f596ede81ae978e6ea56cb
willfrid boris <boriskwayep35@yahoo.com>
2020-09-10 22:04:55 -0400

    :whale2:

--- register.php
+++ register.php
@@ -1,8 +1,6 @@
 <?php
 $msgok=false;
 $msgError='not loaded';
-require 'phpmailer/PHPMailerAutoload.php';
-
 
 if(isset($_POST['Username'],$_POST['Password'],$_POST['Email'])):
     if($_POST['Username']!=""):
@@ -16,36 +14,8 @@ if(isset($_POST['Username'],$_POST['Password'],$_POST['Email'])):
                     $myPDO=new PDO("pgsql:host=ec2-35-171-31-33.compute-1.amazonaws.com;dbname=d5fja2almvc5e8","gxduzwzaxyaymp","388a801b058ba778c16646117bfb17e0426daf5cb118552f96d9688af77741a9");
                     $myPDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                     $verification=$myPDO->query("INSERT INTO utilisateur(name,email,password) VALUES ('$Username','$Email','$Password')");
-                    
+                    $msgError='connected';
                     $msgok=true;
-                    $req=$myPDO->query("SELECT * FROM utilisateur WHERE email='$Email'");
-                    $emit=$req->fetch(PDO::FETCH_ASSOC);
-                    $to=$emit['email'];
-                    $mail=new PHPMailer;
-                    $mail->Host='smtp.gmail.com';
-                    $mail->Port=587;
-                    $mail->SMTPAuth=true;
-                    $mail->SMTPSecure='tls';
-                    $mail->Username='visualinc18';
-                    $mail->Password='LamborginI5';
-                    $mail->setFrom($to);
-                    $mail->addReplyTo(($to));
-                    $mail->isHTML(true);
-                    $mail->Subject='Form Submissiom:Welcome';
-                    $mail->Body='<h1>yo</h1>';
-
-                    if(!$mail->send()){
-                        $msgError='something wrong';
-                    }
-                    else{
-                        $msgError='connected';
-                    }
-
-
-
-
-
-
                     /*
 
                     $req=$myPDO->query("SELECT * FROM utilisateur WHERE email='$Email'");
