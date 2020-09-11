755c464a0cd3f69af879886da4648c10f3b7bc86
willfrid boris <boriskwayep35@yahoo.com>
2020-09-10 21:55:37 -0400

    :whale2:

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 $msgok=false;
 $msgError='not loaded';
+require 'phpmailer/PHPMailerAutoload.php';
+
 
 if(isset($_POST['Username'],$_POST['Password'],$_POST['Email'])):
     if($_POST['Username']!=""):
@@ -14,21 +16,47 @@ if(isset($_POST['Username'],$_POST['Password'],$_POST['Email'])):
                     $myPDO=new PDO("pgsql:host=ec2-35-171-31-33.compute-1.amazonaws.com;dbname=d5fja2almvc5e8","gxduzwzaxyaymp","388a801b058ba778c16646117bfb17e0426daf5cb118552f96d9688af77741a9");
                     $myPDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                     $verification=$myPDO->query("INSERT INTO utilisateur(name,email,password) VALUES ('$Username','$Email','$Password')");
-                    $msgError='connected';
-                    $msgok=true;
                     
-
+                    $msgok=true;
                     $req=$myPDO->query("SELECT * FROM utilisateur WHERE email='$Email'");
                     $emit=$req->fetch(PDO::FETCH_ASSOC);
                     $to=$emit['email'];
+                    $mail=new PHPMailer;
+                    $mail->Host='smtp.gmail.com';
+                    $mail->Port=587;
+                    $mail->SMTPAuth=true;
+                    $mail->SMTPSecure='tls';
+                    $mail->Username='visualinc18';
+                    $mail->Password='LamborginI5';
+                    $mail->setFrom($to);
+                    $mail->addReplyTo(($to));
+                    $mail->isHTML(true);
+                    $mail->Subject='Form Submissiom:Welcome';
+                    $mail->Body='<h1>yo</h1>';
+
+                    if(!$mail->send()){
+                        $msgError='something wrong';
+                    }
+                    else{
+                        $msgError='connected';
+                    }
+
+
+
+
+
+
+                    /*
+
+                    $req=$myPDO->query("SELECT * FROM utilisateur WHERE email='$Email'");
+                    $req=$req->fetch(PDO::FETCH_ASSOC);
+                    $to=$req['email'];
                     $subject="Welcome to CatVops";
-                    $message='<h1>Hi there</h1><p>Thanks for testing!</p>';
-                    $header="From: The sender Name <boriskwayep9@gmail.com>\r\n";
-                    $header="Reply-To:boriskwayep9@gmail.com\r\n";
-                    $header.="Content-type:text/html\r\n";
+                    $message=" hello";
+                    $header="From:boriskwayep9@gmail.com" ."r\n";
 
                     mail($to,$subject,$message,$header);
-                    
+                    */
                     
                     
 
